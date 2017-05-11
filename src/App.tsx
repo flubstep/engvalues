60ea9f275e241c7d3a152829b73e37ffd49a0163
Albert Sheu <albert.sheu@gmail.com>
2017-05-10 22:21:14 -0700

    Add more love

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Component } from 'react';
 import * as React from 'react';
+import { HEARTS } from './store/featureFlags';
+
 // require avoids TS "Could not find a declaration file" errors.
 // Update once DefinitelyTyped includes react-redux 5.0.4
+const _ = require('lodash');
 const { connect } = require('react-redux');
+const FaHeart = require('react-icons/lib/fa/heart');
 
 import ShufflingCardGrid from './components/ShufflingCardGrid';
 import StageInfo from './components/StageInfo';
@@ -38,9 +42,17 @@ class App extends Component<any, any> {
 
   constructor(props: any, context: any) {
     super(props, context);
+    const heartStyles = _.range(100).map(() => (
+      {
+        top: _.random(0, window.innerHeight),
+        left:  _.random(0, window.innerWidth),
+        animation: `heart ${_.random(500, 1500)}ms ease-in-out`
+      }
+    ));
     this.state = {
       windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight
+      windowHeight: window.innerHeight,
+      heartStyles
     };
   }
 
@@ -55,11 +67,31 @@ class App extends Component<any, any> {
     window.addEventListener('resize', this.handleResize);
   }
 
+  renderHearts() {
+    if (HEARTS) {
+      return (
+        <div className="hearts-container">
+          {
+            this.state.heartStyles.map((style: any) => (
+              <div className="heart" style={style}>
+                <FaHeart size={24} fill="red" />
+              </div>
+            ))
+          }
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
   render() {
     const totalWidth = Math.min(this.state.windowWidth, 900);
     const cardWidth = 160;
+
     return (
       <div className="App">
+        {this.renderHearts()}
         <StageInfo {...this.props} className="fake" />
         <ShufflingCardGrid
           width={totalWidth}
--- src/store/featureFlags.tsx
+++ src/store/featureFlags.tsx
@@ -3,3 +3,4 @@ const qs = require('query-string');
 const querystring = qs.parse(window.location.search);
 
 export const LIMIT_TWENTY: Boolean = querystring.ff__limittwenty === '1';
+export const HEARTS: Boolean = querystring.ff__hearts === '1';
