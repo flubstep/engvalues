da802fa319d276eb2664b46f063229aa50b980d1
Jacob Scott <jacob.scott@gmail.com>
2017-05-19 12:12:32 -0700

    Switch to card_limit feature flag

--- src/store/cardsNeeded.ts
+++ src/store/cardsNeeded.ts
@@ -1,8 +1,9 @@
-const CARDS = require('./values.json');
+const CARD_TEXTS = require('./values.json');
+import { NUM_CARDS } from './featureFlags';
 
 const cardsNeeded = (stage: number): number => {
     if (stage === 1) {
-        return Math.floor(CARDS.length / 2);
+        return Math.floor(NUM_CARDS / 2);
     } else {
         return Math.floor(cardsNeeded(stage - 1) / 2);
     }
